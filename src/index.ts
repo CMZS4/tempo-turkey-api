0e47783e4f80e0dd80c993fdec7e94692a693fe1
CMZS4 <84880762+CMZS4@users.noreply.github.com>
2026-03-18 17:47:31 +0300

    railway port düzeltildi

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import path from 'path';
 import { getTCMBRates, getCryptoRates, getAllRates } from './rates';
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
